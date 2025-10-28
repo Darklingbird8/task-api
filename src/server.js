bb72fc5021e329b1049ca4108178053a0b9163e6
Darklingbird8 <tglazenby@gmail.com>
2025-10-28 18:03:43 -0400

    implemented and documented GET /tasks/:id endpoint

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ app.get('/health', (req, res) => {
 app.use('/tasks', taskRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).json({ error: 'Not found' });
+  res.status(404).json({ error: 'Task not found' });
 });
 
 app.use((err, req, res, next) => {
